2b8a79f458afe90865181fb23124081b6d03595a
mgumieniak <maciejgumieniak007@gmail.com>
2020-05-15 13:15:36 +0200

    Add library

---
+++ library-common/src/test/java/library/common/LibraryTest.java
@@ -0,0 +1,16 @@
+package library.common;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LibraryTest {
+
+    @ParameterizedTest(name = "For example, year {0} is not supported.")
+    @ValueSource(booleans = {true, true, true})
+    void name(boolean status) {
+        assertThat(status).isTrue();
+    }
+
+}
--- product_service/src/main/java/com/spring/product_service/ProductServiceApplication.java
+++ product_service/src/main/java/com/spring/product_service/ProductServiceApplication.java
@@ -1,5 +1,6 @@
 package com.spring.product_service;
 
+import library.common.Library;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
